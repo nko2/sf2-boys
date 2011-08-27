70c7f674e7478d2609f888e3ff97979b71def689
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-08-27 11:47:28 -0700

    make fallback to 3000 in dev

--- server.js
+++ server.js
@@ -117,5 +117,5 @@ app.get('/events/1/1', function(req, res){
 });
 
 everyauth.helpExpress(app);
-app.listen(80);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
