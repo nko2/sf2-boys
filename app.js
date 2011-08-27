914fb2d606296fc6b2967f9da32b7538a59e2cec
everzet <ever.zet@gmail.com>
2011-08-27 04:11:03 +0300

    implemented twitter authentication with everyauth

--- app.js
+++ app.js
@@ -3,19 +3,32 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var express   = require('express')
+  , everyauth = require('everyauth')
+  , users     = require('./lib/users');
+
+everyauth.twitter
+    .consumerKey('DROXwWEJw3tXjU4YJpZLw')
+    .consumerSecret('pwv1Nvlvi3PcQ9fwkojiUd933prElu60Iu8FNAonwcI')
+    .findOrCreateUser(function(session, accessToken, accessTokenSecret, userData) {
+        return users.createUserFromTwitterData(userData);
+    })
+    .redirectPath('/');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: "dbe811f8f1b8ea"}));
+  app.use(everyauth.middleware());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.set('view engine', 'ejs');
   app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
 });
 
 app.configure('development', function(){
@@ -29,10 +42,11 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+    res.render('index', {
+        title:  'Express'
+    });
 });
 
+everyauth.helpExpress(app);
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
