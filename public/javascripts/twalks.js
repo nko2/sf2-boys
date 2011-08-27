be1c60c04c23773c396d463815b06fedd6867ac2
everzet <ever.zet@gmail.com>
2011-08-28 00:29:12 +0300

    initial views

--- public/javascripts/twalks.js
+++ public/javascripts/twalks.js
@@ -1,38 +1,64 @@
-$(function(){
+(function($) {
     // Replace ERB-style delimiters for Underscore templates
     _.templateSettings = {
         interpolate: /\{\{(.+?)\}\}/g
       , evaluate:    /\{\%(.+?)\%\}/g
     };
 
-    // Models
     window.Event = Backbone.Model.extend({});
-    window.Talk = Backbone.Model.extend({});
-
-    // Collections
-    window.EventList = Backbone.Collection.extend({
-        model: Event,
-      , url: '/events.json'
-      , comparator: function(event) {
-            return event.get('startsAt');
-        }
-    }
-
-    window.TalkList = Backbone.Collection.extend({
-        model: Talk
-      , url: '/talks.json'
-      , comparator: function(event) {
-            return event.get('name');
-        }
-    }
-
-    // Views
-    window.EventView = Backbone.View.extend({
-        tagName: 'li',
-      , template: _.template($('#event-template').html())
+    window.Events = Backbone.Collection.extend({
+        model:  Event
+      , url:    '/events.json'
     });
 
-    window.Events = new EventList;
-    window.Talks = new TalkList;
-});
+    window.EventsListEventView = Backbone.View.extend({
+        tagName: 'article'
+      , initialize: function() {
+            this.template = _.template($('#events-list-event-template').html());
+        }
+      , render: function() {
+            var content = this.template(this.model.toJSON());
+            $(this.el).html(content);
 
+            return this;
+        }
+    });
+
+    window.EventsListView = Backbone.View.extend({
+        initialize: function() {
+            _.bindAll(this, 'render');
+            this.template = _.template($('#events-list-template').html());
+        }
+      , render: function() {
+            var $list;
+
+            $(this.el).html(this.template({}));
+            $list = this.$('.list');
+
+            this.collection.each(function(event) {
+                var view = new EventsListEventView({
+                    model:       event
+                  , collection:  this.collection
+                });
+
+                $list.append(view.render().el);
+            });
+
+            return this;
+        }
+    });
+
+    $(function(){
+        //$('#bb-content').append(view.render().el);
+    });
+
+    //$(function(){
+    //    $('.nav.secondary-nav a.menu').click(function(ev) {
+    //        $(this).parent().toggleClass('open');
+    //        ev.stopPropagation();
+    //    });
+    //    $('body').click(function() {
+    //        $('.nav.secondary-nav li.menu').removeClass('open');
+    //    });
+    //});
+})(jQuery);
