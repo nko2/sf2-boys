93e538d6fad5b507c18b2313309169d73044afab
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-08-28 13:47:20 -0700

    deploy

--- server.js
+++ server.js
@@ -335,4 +335,4 @@ everyauth.helpExpress(app);
 app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-// deploy please
+// deploy
